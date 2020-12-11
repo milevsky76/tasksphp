1b453ff1c26d94bc86445ed75b176b5023d53270
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-11 11:56:51 +0300

    Adding solution task 1.2
    Adding function getFourDigitValues

--- task1.2.php
+++ task1.2.php
@@ -1,5 +1,26 @@
 <?php //2. Получить  все  четырехзначные  числа,  сумма  цифр  которых  равна заданному числу  n.
 
+function getFourDigitValues(int $number): array
+{
+    $fourDigitNumbers = [];
+    define("LIMIT_FROM", 1000, false);
+    define("LIMIT_TO", 9999, false);
+
+    if($number >= getSumOfNumber(LIMIT_FROM) && $number <= getSumOfNumber(LIMIT_TO)) {
+        for ($i = LIMIT_FROM; $i <= LIMIT_TO ; $i++) {
+            $sumOfNumber = getSumOfNumber($i);
+            
+            if($sumOfNumber === $number) {
+                $fourDigitNumbers[] = $i;
+            }
+        }
+        
+        return $fourDigitNumbers;
+    }
+    
+    return $fourDigitNumbers;
+}
+
 function getSumOfNumber(int $number): int
 {
     $sumOfNumber = 0;

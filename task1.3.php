4ef2b443949338ca6e9a876baf12c4a1c646f492
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-11 14:07:59 +0300

    Adding solution task 1.3
    Adding function isAscendingSequence

--- task1.3.php
+++ task1.3.php
@@ -1,5 +1,29 @@
 <?php //3. Выяснить, образуют ли цифры данного натурального числа N возрастающую последовательность.
 
+function isAscendingSequence(int $naturalNumber): bool
+{
+    $ascendingSequence = false;
+    $countOfDigits = getCountsOfDigits($naturalNumber);
+
+    if($naturalNumber > 0 && $countOfDigits > 1) {
+        while ($countOfDigits != 1) {
+            if($naturalNumber > 9) {
+                $lastNumber = $naturalNumber % 10;
+                $naturalNumber /= 10;
+            }
+
+            if($lastNumber > $naturalNumber % 10) {
+                $ascendingSequence = true;
+                $countOfDigits--;
+            }else {
+                return $ascendingSequence = false;
+            }            
+        }
+    }
+
+    return $ascendingSequence;
+}
+
 function getCountsOfDigits(int $number): int
 {
     $count = ($number == 0) ? 1 : 0;

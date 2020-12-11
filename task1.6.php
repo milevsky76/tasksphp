85008ea3b269a4fa7a9e6d9dab61e41704de073d
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-11 17:56:18 +0300

    Adding solution task 1.6
    Adding function isSpecific

--- task1.6.php
+++ task1.6.php
@@ -1,5 +1,19 @@
 <?php //6. Получить  все  четырехзначные  числа,  в  записи  которых  встречаются только цифры 0,2,3,7.
 
+function isSpecific(int $number, array $specificNumbers): bool
+{
+    for ($j = 0; $j < 4; $j++) { 
+        $lastNumber = $number % 10;
+        $number = (int)($number / 10);
+        
+        if (!isInArray($lastNumber, $specificNumbers)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function isInArray(int $number, array $array): bool
 {
     $numberInArray = false;

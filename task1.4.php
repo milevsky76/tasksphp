d637b499653287483fe796fbfbf779627b7e1bd3
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-11 14:57:53 +0300

    Adding solution task 1.4
    Adding function isAscendingSequence

--- task1.4.php
+++ task1.4.php
@@ -29,6 +29,30 @@ function isDecreasingSequence(int $naturalNumber): bool
     return $decreasingSequence;
 }
 
+function isAscendingSequence(int $naturalNumber): bool
+{
+    $ascendingSequence = false;
+    $countOfDigits = getCountsOfDigits($naturalNumber);
+
+    if($naturalNumber > 0 && $countOfDigits > 1) {
+        while ($countOfDigits != 1) {
+            if($naturalNumber > 9) {
+                $lastNumber = $naturalNumber % 10;
+                $naturalNumber /= 10;
+            }
+
+            if($lastNumber > $naturalNumber % 10) {
+                $ascendingSequence = true;
+                $countOfDigits--;
+            }else {
+                return $ascendingSequence = false;
+            }            
+        }
+    }
+
+    return $ascendingSequence;
+}
+
 function getCountsOfDigits(int $number): int
 {
     $count = ($number == 0) ? 1 : 0;

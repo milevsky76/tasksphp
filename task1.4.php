f0146681ef07664d5f0c5676ac6bd326bb72a8ae
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-11 14:55:21 +0300

    Adding solution task 1.4
    Adding function isDecreasingSequence

--- task1.4.php
+++ task1.4.php
@@ -5,6 +5,30 @@ function isEven(int $number): bool
     return $number % 2 == 0 ? true : false;
 }
 
+function isDecreasingSequence(int $naturalNumber): bool
+{
+    $decreasingSequence = false;
+    $countOfDigits = getCountsOfDigits($naturalNumber);
+
+    if($naturalNumber > 0 && $countOfDigits > 1) {
+        while ($countOfDigits != 1) {
+            if($naturalNumber > 9) {
+                $lastNumber = $naturalNumber % 10;
+                $naturalNumber /= 10;
+            }
+
+            if($lastNumber < $naturalNumber % 10) {
+                $decreasingSequence = true;
+                $countOfDigits--;
+            }else {
+                return $decreasingSequence = false;
+            }            
+        }
+    }
+
+    return $decreasingSequence;
+}
+
 function getCountsOfDigits(int $number): int
 {
     $count = ($number == 0) ? 1 : 0;

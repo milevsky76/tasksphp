59f6b9c7c07126f8c483d52c10476706d42ec364
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-11 15:36:59 +0300

    Adding solution task 1.5
    Adding function getNumberReverseOrder

--- task1.5.php
+++ task1.5.php
@@ -1,5 +1,27 @@
 <?php //4. По заданному натуральному числу N получить число M, записанное цифрами исходного числа, взятыми в обратном порядке.
 
+function getNumberReverseOrder(int $naturalNumber): int
+{
+    $numberReverseOrder = 0;
+
+    if($naturalNumber > 0) {
+        $countsOfDigits = getCountsOfDigits($naturalNumber);    
+
+        while ($countsOfDigits != 0) {
+            if($naturalNumber > 9) {
+                $lastNumber = $naturalNumber % 10;
+                $naturalNumber = (int)($naturalNumber / 10);
+                $numberReverseOrder += $lastNumber * pow(10, --$countsOfDigits);
+            }else {
+                $numberReverseOrder += $naturalNumber;
+                break;
+            }
+        }
+    }
+
+    return $numberReverseOrder;
+}
+
 function getCountsOfDigits(int $number): int
 {
     $count = ($number == 0) ? 1 : 0;

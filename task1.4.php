72a3775af68dd93f7f577e3b032eeebfebfe3b56
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-11 14:53:32 +0300

    Adding solution task 1.4
    Adding function getCountsOfDigits

--- task1.4.php
+++ task1.4.php
@@ -5,4 +5,16 @@ function isEven(int $number): bool
     return $number % 2 == 0 ? true : false;
 }
 
+function getCountsOfDigits(int $number): int
+{
+    $count = ($number == 0) ? 1 : 0;
+
+    while ($number != 0) {
+        $count++;
+        $number = (int)($number / 10);
+    }
+
+    return $count;
+}
+
 ?>

88fd0f3b8441ea529241d8b26b7205f93b6bd1c6
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-16 12:19:18 +0300

    Adding solution task 1.8
    Adding function isSameNumbers

---
+++ task1.8.php
@@ -0,0 +1,21 @@
+<?php //8. Получить все четырехзначные целые числа, в записи которых нет одинаковых цифр.
+
+function isSameNumbers(int $naturalNumber): bool
+{
+    if ($naturalNumber > 9) {
+        do {
+            $lastNumber = $naturalNumber % 10;
+            $residualNaturalNumber = $naturalNumber = (int)($naturalNumber / 10);
+            do {
+                if ($lastNumber == $residualNaturalNumber % 10) {
+                    return true;
+                }
+                $residualNaturalNumber = (int)($residualNaturalNumber / 10);
+            } while ($residualNaturalNumber != 0);
+        } while ($naturalNumber != 0);
+    }
+
+    return false;
+}
+
+?>

6ee5aa8b3afefccf79eef68068f1ec24615159ad
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-16 12:21:23 +0300

    Adding solution task 1.8
    Adding function getFourDigitWithoutIdenticalDigits

--- task1.8.php
+++ task1.8.php
@@ -1,5 +1,21 @@
 <?php //8. Получить все четырехзначные целые числа, в записи которых нет одинаковых цифр.
 
+function getFourDigitWithoutIdenticalDigits(): array
+{
+    $fourDigitWithoutIdenticalDigits = [];
+    define("LIMIT_FROM", 1000, false);
+    define("LIMIT_TO", 9999, false);
+
+    for ($i = LIMIT_FROM; $i <= LIMIT_TO; $i++) { 
+        if(isSameNumbers($i)){
+            continue;
+        }
+        $fourDigitWithoutIdenticalDigits[] = $i;
+    }
+
+    return $fourDigitWithoutIdenticalDigits;
+}
+
 function isSameNumbers(int $naturalNumber): bool
 {
     if ($naturalNumber > 9) {

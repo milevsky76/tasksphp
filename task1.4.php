33384f004ee835209f617c7c8208d74d1c13d2af
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-18 17:10:03 +0300

    Refactoring function
    - Deleting duplicate
    - Change function name

--- task1.4.php
+++ task1.4.php
@@ -1,22 +1,16 @@
 <?php //4. Найти все четные четырехзначные числа, цифры которых следуют в порядке возрастания или убывания.
 
-function getEvenFourDigitAscDescValues(): array
+function getEvenAscDescValues(): array
 {
-    $evenFourDigitAscDecValues = [];
+    $evenAscDecValues = [];
 
-        if(isEven($i)) {
-            if(isDecreasingSequence($i)) {
-                $evenFourDigitAscDecValues[] = $i;
-                continue;
-            }else if(isAscendingSequence($i)) {
-                $evenFourDigitAscDecValues[] = $i;
-                continue;
-            }
     for ($i = 1000; $i <= 9999 ; $i++) {
+        if(isEven($i) && isAscDescSequence($i)) {
+            $evenAscDecValues[] = $i;
         }
     }
     
-    return $evenFourDigitAscDecValues;
+    return $evenAscDecValues;
 }
 
 function isEven(int $number): bool
@@ -24,64 +18,43 @@ function isEven(int $number): bool
     return $number % 2 == 0 ? true : false;
 }
 
-function isDecreasingSequence(int $naturalNumber): bool
+function isAscDescSequence(int $naturalNumber): bool
 {
-    $decreasingSequence = false;
-    $countOfDigits = getCountsOfDigits($naturalNumber);
+    $sequence = false;
+    $asc = false;
+    $desc = false;
 
-    if($naturalNumber > 0 && $countOfDigits > 1) {
-        while ($countOfDigits != 1) {
-            if($naturalNumber > 9) {
-                $lastNumber = $naturalNumber % 10;
-                $naturalNumber /= 10;
+    if ($naturalNumber > 9) {
+        do {
+            $lastNumber = $naturalNumber % 10;
+            $naturalNumber = (int)($naturalNumber / 10);
+
+            if ($lastNumber == $naturalNumber % 10) {
+                $sequence = false;
+                break;
             }
 
-            if($lastNumber < $naturalNumber % 10) {
-                $decreasingSequence = true;
-                $countOfDigits--;
-            }else {
-                return $decreasingSequence = false;
-            }            
-        }
-    }
-
-    return $decreasingSequence;
-}
-
-function isAscendingSequence(int $naturalNumber): bool
-{
-    $ascendingSequence = false;
-    $countOfDigits = getCountsOfDigits($naturalNumber);
-
-    if($naturalNumber > 0 && $countOfDigits > 1) {
-        while ($countOfDigits != 1) {
-            if($naturalNumber > 9) {
-                $lastNumber = $naturalNumber % 10;
-                $naturalNumber /= 10;
+            if ($lastNumber < $naturalNumber % 10) {
+                $desc = true;
+                $sequence = true;
+            } else if ($lastNumber > $naturalNumber % 10) {
+                $asc = true;
+                $sequence = true;
             }
 
-            if($lastNumber > $naturalNumber % 10) {
-                $ascendingSequence = true;
-                $countOfDigits--;
-            }else {
-                return $ascendingSequence = false;
-            }            
-        }
+            if ($asc && $desc) {
+                $sequence = false;
+                break;
+            }
+
+            if ($naturalNumber <= 9) {
+                $sequence;
+                break;
+            }
+        } while ($naturalNumber != 0);
     }
 
-    return $ascendingSequence;
-}
-
-function getCountsOfDigits(int $number): int
-{
-    $count = ($number == 0) ? 1 : 0;
-
-    while ($number != 0) {
-        $count++;
-        $number = (int)($number / 10);
-    }
-
-    return $count;
+    return $sequence;
 }
 
 ?>

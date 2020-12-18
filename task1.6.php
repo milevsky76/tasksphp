0e916eba4b6f04af81dd7a23fbb038baa2537d11
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-18 17:28:33 +0300

    Refactoring function
    - Сhanging function name
    - Сhanging field name
    - Deleting array
    - Deleting function isInArray

--- task1.6.php
+++ task1.6.php
@@ -1,44 +1,31 @@
 <?php //6. Получить  все  четырехзначные  числа,  в  записи  которых  встречаются только цифры 0,2,3,7.
 
-function getSpecificFourDigitNumbers(array $specificNumbers = null): array
+function getSpecificNumbers(): array
 {
-    $specificNumbers = $specificNumbers ?: [0, 2, 3, 7];
-    $specificFourDigitNumbers = [];
+    $specificNumbers = [];
 
     for ($i = 1000; $i <= 9999 ; $i++) {
-        if(isSpecific($i, $specificNumbers)) {
-            $specificFourDigitNumbers[] = $i;
+        if(isSpecific($i)) {
+            $specificNumbers[] = $i;
         }
     }
     
-    return $specificFourDigitNumbers;
+    return $specificNumbers;
 }
 
-function isSpecific(int $number, array $specificNumbers): bool
+function isSpecific(int $number): bool
 {
-    for ($j = 0; $j < 4; $j++) { 
+
+    while ($number != 0) {
         $lastNumber = $number % 10;
         $number = (int)($number / 10);
         
-        if (!isInArray($lastNumber, $specificNumbers)) {
+        if ($lastNumber != 0 && $lastNumber != 2 && $lastNumber != 3 && $lastNumber != 7) {
             return false;
         }
     }
 
     return true;
-}
-
-function isInArray(int $number, array $array): bool
-{
-    $numberInArray = false;
-
-    for ($i = 0; $i < count($array); $i++) {
-        if($number == $array[$i]) {
-            return $numberInArray = true;
-        }
-    }
-
-    return $numberInArray;
 }
 
 ?>

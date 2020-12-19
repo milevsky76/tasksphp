0522e556fb7155e03687ebb814768cdf526366e3
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-19 12:20:01 +0300

    Adding solution task 1.11
    Adding function getDividedSum

--- task1.11.php
+++ task1.11.php
@@ -1,5 +1,19 @@
 <?php //11. Напечатать те элементы последовательности натуральных чисел n0 ,n1,...,nm , которые делятся на сумму своих цифр.
 
+function getDividedSum(int $n, int $m): array
+{
+    $dividedSum = [];
+
+    if (($n > 0 && $m > 0) && $n < $m) {
+        for ($i = $n; $i <= $m; $i++) {
+            if($i % getSumOfNumber($i) == 0){
+                $dividedSum[] = $i;
+            }
+        }
+    }    
+    
+    return $dividedSum;
+}
 
 function getSumOfNumber(int $number): int
 {

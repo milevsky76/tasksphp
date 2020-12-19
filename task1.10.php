b553cccff05eb85066d0a599d339a8a9757a1eab
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-19 10:15:13 +0300

    Adding solution task 1.10
    Adding function getPolyyndromeNumbers

--- task1.10.php
+++ task1.10.php
@@ -1,5 +1,24 @@
 <?php //10. Найти все меньшие N числа-палиндромы, которые при возведении в квадрат дают палиндром. Число называется палиндромом, если его запись читается одинаково с начала и с конца.
 
+function getPolyyndromeNumbers(int $naturalNumber): array
+{
+    $polyyndromeNumbers = [];
+
+    if (--$naturalNumber > 0) {
+        for ($i = $naturalNumber; $i > 0; $i--) {
+            if($i > 9 && !isPalindrome($i, getNumberReverse($i))){
+                continue;
+            }
+
+            if(isPalindrome(pow($i, 2), getNumberReverse(pow($i, 2)))){
+                $polyyndromeNumbers[] = $i;
+            }
+        }
+    }
+    
+    return $polyyndromeNumbers;
+}
+
 function isPalindrome(int $number1, int $number2):bool
 {
     return $number1 === $number2 ? true : false;

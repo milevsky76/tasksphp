5853dd1e3c13438fb69913a63878af205130d05e
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-19 10:14:20 +0300

    Adding solution task 1.10
    Adding function getNumberReverse

--- task1.10.php
+++ task1.10.php
@@ -1,5 +1,27 @@
 <?php //10. Найти все меньшие N числа-палиндромы, которые при возведении в квадрат дают палиндром. Число называется палиндромом, если его запись читается одинаково с начала и с конца.
 
+function getNumberReverse(int $naturalNumber): int
+{
+    $numberReverse = 0;
+
+    if($naturalNumber > 0) {
+        $countsOfDigits = getCount($naturalNumber);    
+
+        while ($countsOfDigits != 0) {
+            if($naturalNumber > 9) {
+                $lastNumber = $naturalNumber % 10;
+                $naturalNumber = (int)($naturalNumber / 10);
+                $numberReverse += $lastNumber * pow(10, --$countsOfDigits);
+            }else {
+                $numberReverse += $naturalNumber;
+                break;
+            }
+        }
+    }
+
+    return $numberReverse;
+}
+
 function getCount(int $number): int
 {
     $count = ($number == 0) ? 1 : 0;

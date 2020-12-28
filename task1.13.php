d74820345f049c8766c0274c83f3e9534a0c44a3
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-12-28 09:56:49 +0300

    Adding solution task 1.13
    Adding function isSymmetrical

--- task1.13.php
+++ task1.13.php
@@ -1,5 +1,20 @@
 <?php //13. Определить, является ли число  2n+m  симметричным, т. е. запись числа содержит четное количество цифр и совпадают его левая и правая половинки.
 
+function isSymmetrical(int $n, int $m): bool
+{
+    $symmetrical = false;
+
+    $number = 2 * $n + $m;
+    
+    $count = getCountsOfDigits($number);
+
+    if($count % 2 == 0 && ((int)($number / pow(10, $count / 2)) == $number % pow(10, $count / 2))){
+        $symmetrical = true;
+    }
+    
+    return $symmetrical;
+}
+
 function getCountsOfDigits(int $number): int
 {
     $count = ($number == 0) ? 1 : 0;

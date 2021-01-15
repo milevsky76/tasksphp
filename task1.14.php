058374c3a1a32130f71e3f1a2c601aecd591f60b
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2021-01-15 14:41:37 +0300

    Adding solution task 1.14
    Adding function getPrimeNumbers

--- task1.14.php
+++ task1.14.php
@@ -1,4 +1,25 @@
 <?php //14. Парными  простыми числами называются два простых числа, разность  которых равна двум. Например,  3 и 5; 11 и 13. Найти 10  парных  простых чисел.
+
+function getPrimeNumbers(int $numberOfPairs = 10): array
+{
+    $num1 = $num2 = 1;
+    $primeNumbers = [];
+
+    do {
+        if (isPrimeNumber($num2)) {
+            if ($num2 - $num1 == 2) {
+                $primeNumbers[] = [$num1, $num2];
+            }
+
+            $num1 = $num2;
+        }
+
+        $num2++;
+    } while (count($primeNumbers) != $numberOfPairs);
+
+    return $primeNumbers;
+}
+
 function isPrimeNumber(int $n): bool
 {
     $primeNumber = false;
